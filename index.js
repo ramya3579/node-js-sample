0eb2282f0116ecdad2f8415120c1295c6fdbfd09
ramya3579 <ramyakota1704@gmail.com>
2025-04-10 11:51:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.get('/', (req, res) => {
+  res.send('Hello from Docker + Jenkins!');
+});
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// ✅ Add this part if it's missing:
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
